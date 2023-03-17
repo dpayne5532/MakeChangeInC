5642878d7ce1a75c24d12f3c7eb078e1b80bd415
Dan Payne <dan@danpayne.info>
2023-03-17 12:48:22 -0600

    update comments

--- makeChange.c
+++ makeChange.c
@@ -8,10 +8,10 @@ gcc -o exeName makeChange.c -lm
 -lm is required to use math library functions floor() and round()
 */
 
-
+// main() is the entry point to the program
 int main() {
 
-    //Decalre our variables
+    // Decalre our variables
     double amount;
     int hundreds, fifties, twenties, tens, fives, ones;
     int quarters, dimes, nickels, pennies;
@@ -19,26 +19,26 @@ int main() {
     printf("\n---===|||  MakeChange takehome project for CEL by Dan Payne |||===---\n");
     printf("Enter an amount in dollars: ");
 
-    //Request user input
-    //%lf will print the Double value, the '&' in '&amount' refers to the memory address of the Double.
+    // Request user input
+    // %lf will print the Double value, the '&' in '&amount' refers to the memory address of the Double.
     scanf("%lf", &amount);
 
-    //Convert dollars to an integer and subtract from amount
-    //floor() rounds but always down
-    //floor(amount) will return a double (int) will typecast that double to int.
+    // Convert dollars to an integer and subtract from amount
+    // floor() rounds but always down
+    // floor(amount) will return a double (int) will typecast that double to int.
     int dollars = (int) floor(amount);
     amount -= dollars;
 
-    //Convert remaining amount to cents
+    // Convert remaining amount to cents
     int cents = (int) round(amount * 100);
 
-    //Calculate number of bills and coins
-    //Hundreds equals dollars value divided by 100
+    // Calculate number of bills and coins
+    // Hundreds equals dollars value divided by 100
     hundreds = dollars / 100;
-    //Dollars variable turns itself into the remaineder of dollars/100 using the modulo operator.
+    // Dollars variable turns itself into the remaineder of dollars/100 using the modulo operator.
     dollars %= 100;
 
-    //Rinse, repeat
+    // Rinse, repeat
     fifties = dollars / 50;
     dollars %= 50;
 
@@ -64,7 +64,7 @@ int main() {
 
     pennies = cents;
 
-    //Print out results
+    // Print out results
     printf("Hundreds: %d\n", hundreds);
     printf("Fifties: %d\n", fifties);
     printf("Twenties: %d\n", twenties);
@@ -76,5 +76,7 @@ int main() {
     printf("Nickels: %d\n", nickels);
     printf("Pennies: %d\n", pennies);
 
+
+  // return 0 just means the program was executed successfully. A return 1 would be unsuccessful.
     return 0;
 }
